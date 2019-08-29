25e23d9962de4a1749aeae0547c84f28bd77443f
José Fernández <fajjaf@gmail.com>
2019-08-28 23:34:33 -0300

    Initial commit

---
+++ app/src/main/java/com/programacionparadispositivosmoviles/lectordesms/MensajeRecibido.java
@@ -0,0 +1,14 @@
+package com.programacionparadispositivosmoviles.lectordesms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MensajeRecibido extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context,"Mensaje Recibido...",Toast.LENGTH_LONG).show();
+    }
+}
